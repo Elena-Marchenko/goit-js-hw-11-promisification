2024ac1d2013112ba503d2dafc375e69663e5279
Elena-Marchenko <Leawest91@gmail.com>
2021-05-12 18:38:41 +0300

    Правки импорта

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const delay = ms => {
-  // Твой код
-};
-
-const logger = time => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+import "./js/promisification_1.js";
+import "./js/promisification_2.js";
+import "./js/promisification_3.js";
