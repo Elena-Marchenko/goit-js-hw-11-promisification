9b9ff357d2e5a8c4513a97516516438cda932105
Elena-Marchenko <Leawest91@gmail.com>
2021-05-12 18:31:19 +0300

    Готовая работа

---
+++ js/promisification_2.js
@@ -0,0 +1,31 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+    return new Promise((resolve) => {
+        resolve(updatedUsers);
+    })
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+
+// Работает так
+
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Должно работать так
+ */
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
